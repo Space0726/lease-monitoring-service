f794534959e611ca28f20d74b51c99ea738181f1
iye <iye@riotgames.com>
2019-01-20 18:21:12 +0900

    Initialize spring-boot application

---
+++ src/main/java/personal/ex0312/kr/lease/LeaseMonitoringServiceApplication.java
@@ -0,0 +1,14 @@
+package personal.ex0312.kr.lease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaseMonitoringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaseMonitoringServiceApplication.class, args);
+	}
+
+}
+
