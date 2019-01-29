11f0c1076444f31c14f6e288385544b86e937327
Ian Ye <iye@riotgames.com>
2019-01-29 21:32:35 +0900

    Add job triggered log

--- src/main/java/personal/ex0312/kr/lease/service/MonitoringJobService.java
+++ src/main/java/personal/ex0312/kr/lease/service/MonitoringJobService.java
@@ -1,6 +1,7 @@
 package personal.ex0312.kr.lease.service;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import personal.ex0312.kr.lease.domain.MonitoringJob;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class MonitoringJobService {
     private final MonitoringJobRepository monitoringJobRepository;
     private final LeaseCollector leaseCollector;
@@ -29,6 +31,7 @@ public class MonitoringJobService {
     @Scheduled(initialDelay = 5000L, fixedDelay = 60000L)
     public void triggerMonitoringJob() {
         List<MonitoringJob> allJobs = this.findAllMonitoringJobs();
+        log.info("Job triggered! the number of job is : {}", allJobs.size());
         leaseCollector.collectLeases(allJobs);
     }
 }
